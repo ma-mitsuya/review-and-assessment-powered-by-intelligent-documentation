0476cbf8d20ff221b498f7b2dc0570d444ae4d04
ma-mitsuya <ma.mitsuya@gmail.com>
2025-08-21 17:23:32 +0900

    Update frontend-waf-stack.ts

--- cdk/lib/frontend-waf-stack.ts
+++ cdk/lib/frontend-waf-stack.ts
@@ -97,6 +97,7 @@ export class FrontendWafStack extends Stack {
 
       this.webAclArn = new cdk.CfnOutput(this, "WebAclId", {
         value: webAcl.attrArn,
+        exportName: `RapidFrontendWafStack-WebAclArn-${props.env?.region}-v2`,
       });
     } else {
       throw new Error(
